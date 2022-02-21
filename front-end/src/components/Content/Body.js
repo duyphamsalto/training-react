c16306c3c1f4cdb83d93f3bf8872b02949270d93
Duy Pham <pham.hong.duy@valuence.inc>
2022-02-21 17:16:18 +0700

    Structure Source

---
+++ front-end/src/components/Content/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+export default function Body() {
+  return (
+    <div className='body'>
+      Body components
+    </div>
+  );
+}
---
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+export default function Sidebar() {
+  return (
+    <div className='side-bar'>
+      Sidebar components
+    </div>
+  );
+}
