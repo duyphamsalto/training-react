d39322af82ea6db5775324314105629bd36fff4d
TaigaHashimoto <taiga.hashimoto@salto.link>
2022-02-25 22:00:11 +0900

    Style: Footerの作成

--- front-end/src/components/Footer/Footer.js
+++ front-end/src/components/Footer/Footer.js
@@ -4,7 +4,11 @@ import './style.scss';
 export default function Footer() {
   return (
     <footer>
-      Footer components
+      <ul>
+        <li><a href="#">プライバシーポリシー</a></li>
+        <li><a href="#">利用規約</a></li>
+      </ul>    
+      <span>&copy; 2022 Admin UI, Made with &hearts; by DUY PHAM Design.</span>
     </footer>
   );
 }
