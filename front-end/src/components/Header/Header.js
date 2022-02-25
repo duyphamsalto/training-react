174923f632ba03b6551e1d1167667af59cfa8c06
TaigaHashimoto <taiga.hashimoto@salto.link>
2022-02-26 00:01:08 +0900

    Style: Header の作成

--- front-end/src/components/Header/Header.js
+++ front-end/src/components/Header/Header.js
@@ -4,7 +4,22 @@ import './style.scss';
 export default function Header() {
   return (
     <header>
-      Header components
+      <div className="search">
+        <input className="font-awesome" type="search" placeholder="&#xF002;&nbsp;Search..."/>
+        <button>検索</button>
+      </div>
+      <div className="contentBox">
+        <img src={`${process.env.PUBLIC_URL}/image/UnionFlag.png`} />
+        <img src={`${process.env.PUBLIC_URL}/image/Notification.png`} />
+        <img src={`${process.env.PUBLIC_URL}/image/Group.png`} />
+        <div className="contentBox__profile">
+          <img src={`${process.env.PUBLIC_URL}/image/profile-image.jpg`} />
+          <div className="contentBox__profile__text">
+            <span>橋本泰河</span>
+            <span>Admin</span>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
