f216fab18b91171c0bed04f8d434a501d0cbd7e0
Taiga17 <taiga.hashimoto@salto.link>
2022-03-02 20:56:33 +0900

    Feat: サイドバー作成

--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -4,7 +4,61 @@ import './style.scss';
 export default function Sidebar() {
   return (
     <div className='side-bar'>
-      Sidebar components
+      <div className="content">
+        <div className="content__logo">
+          <img src={`${process.env.PUBLIC_URL}/image/Logo.png`}/>
+        </div>
+      </div>
+      <div className="content">
+        <div className="content__block dashboard">
+          <a href="#">
+            <img src={`${process.env.PUBLIC_URL}/image/Dashbord.png`} />
+            <span>Dashboard</span>
+          </a>
+        </div>
+      </div>
+      <div className="content">
+        <span className="content__title">Main</span>
+        <div className="content__block">
+          <a href="#">
+            <img src={`${process.env.PUBLIC_URL}/image/3User.png`} />
+            <span>Dashboard</span>
+          </a>
+        </div>
+        <div className="content__block">
+          <a href="#">
+            <img src={`${process.env.PUBLIC_URL}/image/Posts.png`} />
+            <span>Posts</span>
+          </a>
+        </div>
+        <div className="content__block">
+          <a href="#">
+            <img src={`${process.env.PUBLIC_URL}/image/Categories.png`} />
+            <span>Categories</span>
+          </a>
+        </div>
+      </div>
+      <div className="content">
+        <span className="content__title">Settings</span>
+        <div className="content__block">
+          <a href="#">
+            <img src={`${process.env.PUBLIC_URL}/image/Profile.png`} />
+            <span>Profile</span>
+          </a>
+        </div>
+        <div className="content__block">
+          <a href="#">
+            <img src={`${process.env.PUBLIC_URL}/image/Roles.png`} />
+            <span>Roles</span>
+          </a>
+        </div>
+        <div className="content__block">
+          <a href="#">
+            <img src={`${process.env.PUBLIC_URL}/image/Maps.png`} />
+            <span>Maps</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
