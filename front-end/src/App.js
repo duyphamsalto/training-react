cb9f7f653101cea4b5631857380f826085b93b32
Taiga17 <taiga.hashimoto@salto.link>
2022-03-05 02:26:36 +0900

    Feat: ルーティングの設定

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import routes from './routes';
+
 import Body from './components/Content/Body';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import './App.css';
 
 function App() {
   return (
@@ -10,7 +14,15 @@ function App() {
       <Sidebar />
       <div className='contents'>
         <Header />
-        <Body />
+          <Routes>
+            {routes.map((route, idx) => (
+              <Route
+                path={route.path}
+                element={route.component}
+                key={idx}
+              />
+            ))}
+          </Routes>
         <Footer />
       </div>
     </div>
