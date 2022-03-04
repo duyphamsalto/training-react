00a54c5f835a33b177f5d77f5152198dfac7cd3a
Taiga17 <taiga.hashimoto@salto.link>
2022-03-05 03:28:57 +0900

    Feat: ルーティングの設定

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,7 +1,11 @@
+import Body from './components/Content/Body';
 import Dashboard from './components/Content/Dashboard';
 import Users from './components/Content/Users';
 import Posts from './components/Content/Posts';
-import Body from './components/Content/Body';
+import Categories from './components/Content/Categories';
+import Profile from './components/Content/Profile';
+import Roles from './components/Content/Roles';
+import Maps from './components/Content/Maps';
 
 const routes = [
   {
@@ -16,6 +20,18 @@ const routes = [
   }, {
     path: '/posts',
     component: <Posts />
+  }, {
+    path: '/categories',
+    component: <Categories />
+  }, {
+    path: '/profile',
+    component: <Profile />
+  }, {
+    path: '/roles',
+    component: <Roles />
+  }, {
+    path: '/maps',
+    component: <Maps />
   },
 ];
   
