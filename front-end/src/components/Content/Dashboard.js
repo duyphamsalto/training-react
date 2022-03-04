dcfa66275181a368c067417ba75b4d4a8b81720b
Taiga17 <taiga.hashimoto@salto.link>
2022-03-05 02:28:54 +0900

    Feat: Dashboard ページの作成

---
+++ front-end/src/components/Content/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/dashboard.scss';
+
+export default function Dashboard() {
+    return (
+      <div className='dashboard'>
+        Dashboard components
+      </div>
+    );
+  }
