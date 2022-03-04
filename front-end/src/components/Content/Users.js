8ae65008880e9153e2bcf3253d4da3cd823957b5
Taiga17 <taiga.hashimoto@salto.link>
2022-03-05 02:27:52 +0900

    Feat: Users ページの作成

---
+++ front-end/src/components/Content/Users.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/users.scss';
+
+export default function Users() {
+    return (
+      <div className='users'>
+        Users components
+      </div>
+    );
+  }
