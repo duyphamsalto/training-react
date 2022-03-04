ce1c1f74361a90f67d530b343d6ec52cfefb9d49
Taiga17 <taiga.hashimoto@salto.link>
2022-03-05 02:29:32 +0900

    Feat: Posts ページの作成

---
+++ front-end/src/components/Content/Posts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/posts.scss';
+
+export default function Posts() {
+    return (
+      <div className='posts'>
+        Posts components
+      </div>
+    );
+  }
