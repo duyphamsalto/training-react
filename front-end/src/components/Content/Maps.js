4d7c18e30490788e146632aa57d7cc57f5c2c5fc
Taiga17 <taiga.hashimoto@salto.link>
2022-03-05 03:26:08 +0900

    Feat: Maps ページの作成

---
+++ front-end/src/components/Content/Maps.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/maps.scss';
+
+export default function Maps() {
+  return (
+    <div id="maps" className="">
+      <span>Maps</span>
+    </div>
+  );
+}
