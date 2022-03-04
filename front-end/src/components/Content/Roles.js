b05d5a4f04c868d1fc25b12c4bacbaceb931012b
Taiga17 <taiga.hashimoto@salto.link>
2022-03-05 03:28:42 +0900

    Feat: Roles ページの作成

---
+++ front-end/src/components/Content/Roles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/roles.scss';
+
+export default function Roles() {
+    return (
+      <div id="roles" className="">
+        <span>Roles</span>
+      </div>
+    );
+  }
