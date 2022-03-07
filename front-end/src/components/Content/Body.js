2f01e608d246c659b2c4d67b44e333e0e4e80605
kuwabarakatsuyoshi <katsuyoshi.kuwabara@salto.link>
2022-03-07 21:17:15 +0900

    Changed the content to be displayed and the way CSS is read.

--- front-end/src/components/Content/Body.js
+++ front-end/src/components/Content/Body.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import './style.scss';
+import './style.css';
+import '../../Assets/css/cardStyle.css';
 
 export default function Body() {
+  const description = "TextTextTextTextTextTextTextTextTextTextText TextTextTextTextTextTextTextTextTextTextText TextTextTextTextTextTextTextTextTextTextText"
+
+  function Card(params){
+    let result = <div className='card'> <img src={params.pass} alt="Test"></img>
+                   <div className='textArea'><h2>{params.title}</h2>
+                   <p>{params.text}</p></div></div>
+    return result
+  }
+
+
   return (
     <div className='body'>
-      Body components
+      <div className='App'>
+        <div className='itemArea'>
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+          <Card pass="https://placehold.jp/256x144.png" text={description} title="Title" />
+        </div>
+      </div>
     </div>
   );
 }
