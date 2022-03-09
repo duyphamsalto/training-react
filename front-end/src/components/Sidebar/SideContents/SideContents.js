0af5abc280a5fd92408999c32bd4af1733ad4e5b
Taiga17 <taiga.hashimoto@salto.link>
2022-03-10 01:07:45 +0900

    Refactor: Refactoring Sidebar

---
+++ front-end/src/components/Sidebar/SideContents/SideContents.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useState, useCallback } from 'react';
+import { Link, useParams, NavLink } from "react-router-dom";
+// import './style.scss';
+
+export default function SideContents(props) {
+  // 文字列の先頭を大文字にする処理
+  const capitalize = function(str) {
+    if (typeof str !== 'string' || !str) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  };
+
+  return(
+    <div className="sidebar__content">
+      <div className='sidebar__content__title'>{capitalize(props.title)}</div>
+      {props.block.map((b) => (
+        <div className={`sidebar__content__block ${b}`}>
+          <NavLink to={`/${b}`} className={({isActive}) => (isActive ? 'activated':'')}>
+            <img src={`${process.env.PUBLIC_URL}/image/${capitalize(b)}.png`} />
+            {capitalize(b)}
+          </NavLink>
+        </div>
+      ))}
+    </div>
+  )
+}
--- front-end/src/components/Sidebar/Sidebar.js
+++ front-end/src/components/Sidebar/Sidebar.js
@@ -1,59 +1,36 @@
 import React from 'react';
 import { useState, useCallback } from 'react';
-import { Link, useParams } from "react-router-dom";
 import './style.scss';
+
+import SideContents from './SideContents/SideContents'
 
 export default function Sidebar() {
 
   const menu = [
     {
+      "id"    : "1",
       "title" : "",
       "block" : [ "dashboard" ]
     }, {
+      "id"    : "2",
       "title" : "main",
       "block" : [ "users", "posts", "categories" ]
     }, {
+      "id"    : "3",
       "title" : "settings",
       "block" : [ "profile", "roles", "maps" ]
     }
   ]
 
-  // 再レンダリングさせる関数
-  const useForceUpdate = () => {
-    const [ignored, newState] = useState();
-    return useCallback(() => newState({}), []);
-  }
-
-  // 文字列の先頭を大文字にする処理
-  const capitalize = function(str) {
-    if (typeof str !== 'string' || !str) return str;
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-  };
-
-  const thisPage = function(str) {
-    const params = window.location.pathname.slice(1);
-    return ( params == str ? ' active' : '' );
-  }
-
   return (
-    <div className='side-bar'>
-      <div className="content">
-        <div className="content__logo">
+    <div className='sidebar'>
+      <div className="sidebar__content">
+        <div className="sidebar__content__logo">
           <img src={`${process.env.PUBLIC_URL}/image/Logo.png`}/>
         </div>
       </div>
       {menu.map((m) => (
-        <div className="content">
-          <span className="content__title">{capitalize(m.title)}</span>
-          {m.block.map((b) => (
-            <div className={`content__block ${b}`}>
-              <Link className={thisPage(b)} to={b} onClick={useForceUpdate}>
-                <img src={`${process.env.PUBLIC_URL}/image/${capitalize(b)}.png`} />
-                <span>{capitalize(b)}</span>
-              </Link>
-            </div>
-          ))}
-        </div>
+        <SideContents key={m.id} title={m.title} block={m.block} />
       ))}
     </div>
   );
