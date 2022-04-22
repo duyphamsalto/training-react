22a62c911a97c34b0e13a73107498b1a416df876
Taiga17 <taiga.hashimoto@salto.link>
2022-04-22 16:48:44 +0900

    Feat: ログイン画面の作成

--- front-end/src/components/Contents/Login/index.js
+++ front-end/src/components/Contents/Login/index.js
@@ -1,8 +1,76 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
+import { Box, Container, Typography, Stack, Avatar, TextField, Button, Link,} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import SendIcon from "@mui/icons-material/Send";
 export default function Login() {
-
-    return(
-        <>Login</>
-    );
+  return(
+    <>
+      <Container
+        maxWidth="xs"
+        sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        }}
+      >
+        <Box sx={{ height: "100vh", width: "100%" }}>
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={3}
+            py={6}
+          >
+            <Avatar
+              sx={{
+                bgcolor: "primary.main",
+                "&.MuiAvatar-root": {
+                  fontSize: "30px",
+                },
+              }}
+            >
+              <LockOutlinedIcon fontSize="large" />
+            </Avatar>
+            <Typography variant="h3">Sign in</Typography>
+            <TextField
+              required
+              fullWidth
+              id="email"
+              type="email"
+              label="Email Address"
+              variant="outlined"
+              name="email"
+            />
+            <TextField
+              required
+              id="outlined-password-input"
+              type="password"
+              label="Password"
+              variant="outlined"
+              fullWidth
+              name="password"
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              endIcon={<SendIcon />}
+              fullWidth
+              sx={{ fontSize: 14 }}
+            >SIGN IN</Button>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              spacing={12}
+              sx={{ width: "100%" }}
+            >
+              <Link href="#" variant="body2">Forgot password?</Link>
+              <Link href="#" variant="body2">Sign up now?</Link>
+            </Stack>
+          </Stack>
+        </Box>
+      </Container>
+    </>
+  );
 }
