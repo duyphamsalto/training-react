e149182679c991446a3aabd000f7e91577ed8a9d
Taiga17 <taiga.hashimoto@salto.link>
2022-04-28 17:17:23 +0900

    Feat: 整理

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './store'
+import { Provider } from 'react-redux'
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,
   </BrowserRouter>,
   document.getElementById('root')
 );
